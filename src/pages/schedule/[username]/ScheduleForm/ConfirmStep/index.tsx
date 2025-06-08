844613bbcf8f8d1940461328ddce9456bd3a5086
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-06-08 14:04:04 -0300

    #D275 40 - Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,41 @@
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { CalendarBlank, Clock } from "phosphor-react";
+
 export function ConfirmStep() {
-  return <div />;
+  function handleConfirmSheduling() {}
+
+  return (
+    <ConfirmForm>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size="sm">Nome Completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  );
 }
