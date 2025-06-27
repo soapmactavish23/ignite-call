18a6eb8c3798eff74e91846ee33e7a60e772d465
henrick.nogueira <henrick.nogueira@cnpq.br>
2025-06-27 19:36:43 -0300

    #D290 55 - Fluxo da confirmação

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -17,7 +17,11 @@ interface Availability {
   availableTimes: number[];
 }
 
-export function CalendarStep() {
+interface CalendarStepProps {
+  onSelectedDateTime: (date: Date) => void;
+}
+
+export function CalendarStep({ onSelectedDateTime }: CalendarStepProps) {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
   const router = useRouter();
@@ -48,6 +52,11 @@ export function CalendarStep() {
     enabled: !!selectedDate,
   });
 
+  function handleSelectTime(hour: number) {
+    const dateWithTime = dayjs(selectedDate).set("hour", hour).startOf("hour").toDate();
+    onSelectedDateTime(dateWithTime);
+  }
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
@@ -63,6 +72,7 @@ export function CalendarStep() {
               return (
                 <TimePickerItem
                   key={hour}
+                  onClick={() => handleSelectTime(hour)}
                   disabled={!availability.availableTimes.includes(hour)}
                 >
                   {String(hour).padStart(2, "0")}:00h
